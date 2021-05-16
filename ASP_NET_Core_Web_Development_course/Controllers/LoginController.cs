c556e0c722b5e0dfdb3b35d1e60cef07eb2980cd
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-05-16 23:13:39 +0200

    login form

---
+++ ASP_NET_Core_Web_Development_course/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Core_Web_Development_course.Models;
+
+namespace ASP_NET_Core_Web_Development_course.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "Bill" && userModel.Password == "bigbuck")
+            {
+                return View("LoginSuccess",userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+           
+        }
+    }
+}
