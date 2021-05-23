256995feab6ebd22b5809c0bff628625a3e5991b
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-05-24 01:21:06 +0200

    Revert "user authentication with sql database"
    This reverts commit 565b77b9e7f057d783390b69f1d9789a2473e8b6.

--- ASP_NET_Core_Web_Development_course/Services/SecurityService.cs
+++ ASP_NET_Core_Web_Development_course/Services/SecurityService.cs
@@ -8,23 +8,18 @@ namespace ASP_NET_Core_Web_Development_course.Services
 {
     public class SecurityService
     {
-        //private List<UserModel> knownUsers = new List<UserModel>();
-
-        private UsersDAO userDao = new UsersDAO();
-
+        private List<UserModel> knownUsers = new List<UserModel>();
         //ctor to konstruktor
-        //public SecurityService()
-        //{
-        //    knownUsers.Add(new UserModel {Id=0, UserName = "Bill", Password = "bigbucks"});
-        //    knownUsers.Add(new UserModel { Id = 1, UserName = "John", Password = "absolut" });
-        //    knownUsers.Add(new UserModel { Id = 2, UserName = "Elon", Password = "boomer" });
-
-        //}
+        public SecurityService()
+        {
+            knownUsers.Add(new UserModel {Id=0, UserName = "Bill", Password = "bigbucks"});
+            knownUsers.Add(new UserModel { Id = 1, UserName = "John", Password = "absolut" });
+            knownUsers.Add(new UserModel { Id = 2, UserName = "Elon", Password = "boomer" });
+        }
 
         public bool IsValid(UserModel user)
         {
-            //return knownUsers.Any(x => x.UserName == user.UserName && x.Password==user.Password);
-            return userDao.FindUserByNameAndPassword(user);
+            return knownUsers.Any(x => x.UserName == user.UserName && x.Password==user.Password);
         }
     }
 }
