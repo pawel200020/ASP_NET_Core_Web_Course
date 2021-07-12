32f3f7b74989c9d44d5cb22bacb64bd22f8f4113
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-07-12 19:55:44 +0200

    Solution Refactory, new project added

---
+++ Entity_Freamework_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entity_Freamework_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
