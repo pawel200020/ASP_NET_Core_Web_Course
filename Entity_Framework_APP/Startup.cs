b159629aee731598712661f25689546962bfc9d2
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-07-12 20:11:51 +0200

    New rev of second app

---
+++ Entity_Framework_APP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entity_Framework_APP.Startup))]
+namespace Entity_Framework_APP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
