ed7baa710f2fe4fa7da85c9d39616f4ebfd493b7
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-08-11 16:20:24 +0200

    ButtonGrid appears, prev projects finishing touches

---
+++ ButtonGrid/Controllers/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        private Random random = new Random();
+        private const int SIZE = 25;
+        public IActionResult Index()
+        {
+            if (buttons.Count < SIZE)
+            {
+                for (int i = 0; i < SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel(i, random.Next(4)));
+
+                }
+            }
+            
+            
+            return View("Index",buttons);
+        }
+
+        public IActionResult HandleButtonClick(string number)
+        {
+            int num = int.Parse(number);
+            buttons.ElementAt(num).State = (buttons.ElementAt(num).State + 1)%4;
+            return View("Index", buttons);
+        }
+    }
+}
---
+++ ButtonGrid/Models/ButtonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+        public int State { get; set; }
+
+        public ButtonModel(int id, int state)
+        {
+            this.Id = id;
+            this.State = state;
+        }
+    }
+}
