90b12b85d0dc18a566b59fcb233782a49374b5ae
Paweł Pyciński <pawel200020@gmail.com>
2021-10-10 23:28:49 +0200

    Product APP - DTO in rest api added

--- Basic_Products_APP/Controllers/ProductControllerAPI.cs
+++ Basic_Products_APP/Controllers/ProductControllerAPI.cs
@@ -8,6 +8,7 @@ using ASP_NET_Core_Web_Development_course.Models;
 using ASP_NET_Core_Web_Development_course.Services;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Bogus;
+using System.Web.Http.Description;
 
 namespace ASP_NET_Core_Web_Development_course.Controllers
 {
@@ -15,33 +16,62 @@ namespace ASP_NET_Core_Web_Development_course.Controllers
     [Route("api/")]
     public class ProductControllerAPI : ControllerBase
     {
+        //[HttpGet]
+        //public ActionResult<IEnumerable<ProductModelDTO>> Index()
+        //{
+        //    //HardCodedSampleDataRepository hardCodedSampleDataRepository = new HardCodedSampleDataRepository();
+        //    //return View(hardCodedSampleDataRepository.GetAllProducts());
+        //    ProductsDAO products = new ProductsDAO();
+        //    List<ProductModel> productsList = products.GetAllProducts();
+        //    List<ProductModelDTO> productListExt = new List<ProductModelDTO>();
+        //    //first idea
+        //    foreach (var item in productsList)
+        //    {
+        //        productListExt.Add(new ProductModelDTO(item));
+        //    }
+        //    //another idea is to use link laguage i c#
+        //    //IEnumerable<ProductModelDTO> ProductListExt2 = from p in productsList select new ProductModelDTO(p);
+        //    return productListExt;
+        //}
+
         [HttpGet]
-        public ActionResult<IEnumerable<ProductModel>> Index()
+        [ResponseType(typeof(List<ProductModelDTO>))]
+        public IEnumerable<ProductModelDTO> Index()
         {
-            //HardCodedSampleDataRepository hardCodedSampleDataRepository = new HardCodedSampleDataRepository();
-            //return View(hardCodedSampleDataRepository.GetAllProducts());
             ProductsDAO products = new ProductsDAO();
-            return products.GetAllProducts();
+            List<ProductModel> productsList = products.GetAllProducts();
+            //another idea is to use link laguage i c#
+            IEnumerable<ProductModelDTO> ProductListExt = from p in productsList select new ProductModelDTO(p);
+            return ProductListExt;
         }
+
+
+
         [HttpGet("searchproducts/{searchTerm}")]
-        public ActionResult<IEnumerable<ProductModel>> Search(string searchTerm)
+        [ResponseType(typeof(List<ProductModelDTO>))]
+        public IEnumerable<ProductModelDTO> Search(string searchTerm)
         {
             ProductsDAO products = new ProductsDAO();
             List<ProductModel> productList = products.SearchProducts(searchTerm);
-            return productList;
+            IEnumerable<ProductModelDTO> ProductListExt = from p in productList select new ProductModelDTO(p);
+            return ProductListExt;
+
+           
         }
         [HttpGet("ShowDetails/{Id}")]
-        public ActionResult<ProductModel> ShowDetails(int id)
+        public ActionResult<ProductModelDTO> ShowDetails(int id)
         {
             ProductsDAO product = new ProductsDAO();
             ProductModel foundProduct = product.GetProductById(id);
-            return foundProduct;
+            ProductModelDTO foundProductExt = new ProductModelDTO(foundProduct);
+            return foundProductExt;
         }
         [HttpPost("ProcessCreate")]
         public ActionResult <int> ProcessCreate(ProductModel product)
         {
             ProductsDAO products = new ProductsDAO();
             int newID = products.Insert(product);
+
             return newID;
         }
         [HttpPut("ProcessEdit")]
---
+++ Basic_Products_APP/Models/ProductModelDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core_Web_Development_course.Models
+{
+    public class ProductModelDTO
+    {
+        [DisplayName("Id number")]
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        [DisplayName("Cost to costumer")]
+        [DataType(DataType.Currency)]
+        public decimal price { get; set; }
+        [DisplayName("what you get")]
+        public string PriceString { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Tax { get; set; }
+        public ProductModelDTO(int id, string name, decimal price, string description)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.price = price;
+            this.Description = description;
+
+            PriceString = string.Format("{0:C}", price);
+            ShortDescription = description.Length <= 25 ? description : description.Substring(0, 25);
+            Tax = price * 0.23M;
+
+        }
+        public ProductModelDTO(ProductModel product)
+        {
+            this.Id = product.Id;
+            this.Name = product.Name;
+            this.price = product.price;
+            this.Description = product.Description;
+
+            PriceString = string.Format("{0:C}", price);
+            ShortDescription = product.Description.Length <= 25 ? product.Description : product.Description.Substring(0, 25);
+            Tax = price * 0.23M;
+
+        }
+    }
+}
