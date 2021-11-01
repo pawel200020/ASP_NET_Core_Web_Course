8c14f88dcf38891cfa884cb32e3a29f49d14d2fd
Paweł Pyciński <pawel200020@gmail.com>
2021-11-01 23:09:38 +0100

    logger added

--- Basic_Products_APP/Controllers/LoginController.cs
+++ Basic_Products_APP/Controllers/LoginController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASP_NET_Core_Web_Development_course.Models;
 using ASP_NET_Core_Web_Development_course.Services;
+using Microsoft.Extensions.Logging;
+using NLog;
 
 namespace ASP_NET_Core_Web_Development_course.Controllers
 {
     public class LoginController : Controller
     {
+        private static Logger logger = LogManager.GetLogger("RegisterLoginAppRule");
         public IActionResult Index()
         {
             return View();
@@ -17,13 +20,18 @@ namespace ASP_NET_Core_Web_Development_course.Controllers
 
         public IActionResult ProcessLogin(UserModel userModel)
         {
+            logger.Info("Processing loging attepted");
+            logger.Info(userModel.toString);
+
             SecurityService securityService = new SecurityService();
             if (securityService.IsValid(userModel))
             {
+                logger.Info("Login Success");
                 return View("LoginSuccess",userModel);
             }
             else
             {
+                logger.Warn("Login failure");
                 return View("LoginFailure", userModel);
             }
            
--- Basic_Products_APP/Models/UserModel.cs
+++ Basic_Products_APP/Models/UserModel.cs
@@ -10,5 +10,9 @@ namespace ASP_NET_Core_Web_Development_course.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string toString()
+        {
+            return "name: " + UserName + "pass: " + Password;
+        }
     }
 }
